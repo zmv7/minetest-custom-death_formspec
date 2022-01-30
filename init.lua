bce20dbbad0c7ee16b2365794e31f704334928b6
zmv7 <72821250+zmv7@users.noreply.github.com>
2022-01-31 02:55:43 +0500

    Little cosmetic changes)

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local dformspec = "size[4,3.5]bgcolor[#320000b4;true]" ..
+local formspec = "size[4,3.5]" ..
 		"label[1.45,0;" .. fgettext("You died") ..
 		"]button_exit[1,1;2,0.5;btn_respawn;".. fgettext("Respawn") ..
 		"]button_exit[1,2;2,0.5;btn_ghostmode;" .. "GhostMode" .. 
@@ -6,11 +6,11 @@ local dformspec = "size[4,3.5]bgcolor[#320000b4;true]" ..
 
 core.register_on_death(function()
 	core.display_chat_message(minetest.colorize("#F00","You died at " .. core.pos_to_string(vector.round(core.localplayer:get_pos()))))
-	core.show_formspec("df", dformspec)
+	core.show_formspec("cdf", formspec)
 end)
 
 core.register_on_formspec_input(function(formname, fields)
-	if formname == "df" then
+	if formname == "cdf" then
 		if fields.btn_disconnect then
 		    core.disconnect()
 		elseif fields.btn_respawn then
