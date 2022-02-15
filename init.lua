629e530e2d921e5d77430ee6577287bf1e77bfe2
zmv7 <72821250+zmv7@users.noreply.github.com>
2022-02-15 14:28:14 +0500

    Added waypoint

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
+local DWP = nil
+local DP
 core.register_on_death(function()
-	core.display_chat_message(minetest.colorize("#F00","You died at " .. core.pos_to_string(vector.round(core.localplayer:get_pos()))))
+    DP = core.localplayer:get_pos()
+	core.display_chat_message(minetest.colorize("#F00","You died at " .. core.pos_to_string(vector.round(DP))))
 	core.show_formspec("cdf", "size[4,3.5]" ..
 		"label[1.45,0;" .. fgettext("You died") ..
 		"]button_exit[1,1;2,0.5;btn_respawn;".. fgettext("Respawn") ..
@@ -13,6 +16,17 @@ core.register_on_formspec_input(function(formname, fields)
 		    core.disconnect()
 		elseif fields.btn_respawn then
 		    core.send_respawn()
+         if not DWP then
+            DWP = core.localplayer:hud_add({
+            hud_elem_type = "waypoint",
+            name = "DeathPoint",
+            number = 0xFF0000,
+            world_pos = DP})
+            if not DWP then return end
+        else
+            core.localplayer:hud_change(DWP,'world_pos',DP)
+        end
+
 		else
 		    core.display_chat_message(minetest.colorize("#FF0","GhostMode active. Type '.resp' to respawn."))
 		end
@@ -25,7 +39,15 @@ core.register_chatcommand("resp", {
 		if core.localplayer:get_hp() == 0 then
 			core.send_respawn()
 		else
-			core.display_chat_message(minetest.colorize("#FF0","You're alive yet;)"))
+			core.display_chat_message(minetest.colorize("#FF0","You're alive yet ;)"))
 		end
-	end
-})
+end})
+
+core.register_chatcommand("rdp", {
+	description = "Remove death waypoint",
+	func = function()
+        if DWP then
+            core.localplayer:hud_remove(DWP)
+            DWP = nil
+        end
+end})
